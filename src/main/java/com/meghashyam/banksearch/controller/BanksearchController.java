b2590840914c7d98fb63d3d04eded49aacbabd12
meghashyamc <c.meghashyam@gmail.com>
2019-08-20 09:45:53 +0530

    update my app

--- src/main/java/com/meghashyam/banksearch/controller/BanksearchController.java
+++ src/main/java/com/meghashyam/banksearch/controller/BanksearchController.java
@@ -4,17 +4,34 @@ import com.meghashyam.banksearch.model.Banksearch;
 import com.meghashyam.banksearch.service.LimitOffsetSortService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api")
 
 public class BanksearchController {
     @Autowired
     private LimitOffsetSortService limitOffsetSortService;
 
-    @GetMapping(value = "/branches", params = {"q", "limit", "offset"})
+    @GetMapping(value = "/")
+    public ResponseEntity index1() {
+
+        return ResponseEntity.ok().body("Welcome! Queries need to be sent in the format /api/branches?q=QUERY&limit=LIMIT&offset=OFFSET or" +
+                " in the format /api/branches/autocomplete?q=QUERY&limit=LIMIT&offset=OFFSET");
+    }
+
+    @GetMapping(value = "/api")
+    public ResponseEntity index2() {
+
+        return ResponseEntity.ok().body("Welcome! Queries need to be sent in the format /api/branches?q=QUERY&limit=LIMIT&offset=OFFSET or" +
+                " in the format /api/branches/autocomplete?q=QUERY&limit=LIMIT&offset=OFFSET");
+    }
+
+    @GetMapping(value = "/api/branches", params = {"q", "limit", "offset"})
     @ResponseBody
 
     public Page<Banksearch> findJsonDataByKeyword(@RequestParam("q") String q,
@@ -25,7 +42,7 @@ public class BanksearchController {
 
     }
 
-    @GetMapping(value = "/branches/autocomplete", params = {"q", "limit", "offset"})
+    @GetMapping(value = "/api/branches/autocomplete", params = {"q", "limit", "offset"})
     @ResponseBody
 
     public Page<Banksearch> findJsonDataByBranchPrefix(@RequestParam("q") String q,
